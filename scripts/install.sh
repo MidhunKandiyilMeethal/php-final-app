df0a87ea26f6e77f7157f1bca90d78a3fc590e01
MidhunKandiyilMeethal <mkm763@gmail.com>
2026-09-16 17:48:04 +0530

    Simplify install script and include PHP installation

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "Starting install" > /tmp/deploy.log
-sudo yum install -y httpd >> /tmp/deploy.log 2>&1 || true
-sudo systemctl start httpd || true
-sudo systemctl enable httpd || true
-sudo chmod -R 777 /var/www/html
-echo "Install OK" >> /tmp/deploy.log
+yum install -y httpd php || true
 exit 0
